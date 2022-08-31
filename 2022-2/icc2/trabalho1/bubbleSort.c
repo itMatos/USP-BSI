1569aed40e606d1f8a526eaa2ea072e72149ebfe
Italo <italomatos764@gmail.com>
2022-08-31 11:09:03 -0300

    Algoritmo bubbleSort para o trabalho 1

---
+++ 2022-2/icc2/trabalho1/bubbleSort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int *indexA, int *indexB) {
+  int aux = *indexA;
+  *indexA = *indexB;
+  *indexB = aux;
+}
+
+int bubbleSort(int *array, int size) {
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = 0; j < size - i - 1; j++) {
+      printf("C %d %d\n", j, j+1);
+      
+      if (array[j] > array[j + 1]) {
+        printf("T %d %d\n", j, j+1);
+        swap(&array[j], &array[j + 1]);
+      }
+    }
+  }
+}
+ 
+void printArray(int *array, int size) {
+  for (int i = 0; i < size; i++){
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int tamanho = 4;
+  int vet[] = { 3, 6, 5, 2 };
+
+  bubbleSort(vet, tamanho);
+  printArray(vet, tamanho);
+}
