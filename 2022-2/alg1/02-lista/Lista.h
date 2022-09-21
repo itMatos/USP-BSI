bf6d0357cb86e8d74ea32a1fed75bbc6e351136a
Italo <italomatos764@gmail.com>
2022-09-21 10:50:46 -0300

    git ignore

---
+++ 2022-2/alg1/02-lista/Lista.h
@@ -0,0 +1,30 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#define MAX_TAM 100
+
+#define JA_EXISTE -2
+#define NAO_ENCONTROU -1
+#define ERRO_CHEIA 0
+#define SUCESSO 1
+
+typedef int t_chave;
+typedef int t_apontador;
+
+typedef struct {
+  t_chave chave;
+  char nome[50];
+} t_elemento;
+
+typedef struct {
+  t_elemento elementos[MAX_TAM];
+  t_apontador ultimo;
+} t_lista;
+
+int criar(t_lista *lista);
+int inserir(t_lista *lista, t_elemento elemento);
+int vazia(t_lista *lista);
+int cheia(t_lista *lista);
+void imprimir(t_lista *lista);
+
+#endif // !LISTA_H
